46dbab47895a71a2bd362f879eb9f8d4617aa047
Justin Le <justin@jle.im>
2015-03-08 15:27:49 -0700

    tweaks to work with IRC backend

--- src/Main.hs
+++ src/Main.hs
@@ -2,16 +2,17 @@ module Main where
 
 import System.Environment
 import Auto.Chatbot.Backend.Stdin
+import Auto.Chatbot.Backend.IRC
 import Module.Karma
 import Auto.Chatbot
 
+channels :: [Channel]
+channels = ["#jlebot-test","#ucsd","#haskell-auto"]
+
 main :: IO ()
-main = do
-    modes <- getArgs
-    stdinLoopChron "justin" "#stdin" 1000000 chatbot
-    -- if "irc" `elem` modes
-    --   then undefined
-    --   else stdinLoop chatbot
+main =
+    withIrc "irc.freenode.org" "jlebot2" channels True 1000000 chatbot
+    -- stdinLoopChron "justin" "#stdin" 1000000 chatbot
 
 chatbot :: Monad m => ChatBot m
 chatbot = fromRoom karmaBot
